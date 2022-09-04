624ef6214bc2cd34b04c2f36b7d3c736b5042e84
Drago286 <drago.peric@alumnos.ucn.cl>
2022-09-04 13:04:32 -0400

    Avances

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       Log.e("MainActivity", "Happy Birthday");
+       Log.d("MainActivity", "Happy Birthday");
+       Log.i("MainActivity", "Happy Birthday");
+       Log.v("MainActivity", "Happy Birthday");
+       Log.w("MainActivity", "Happy Birthday");
+
+
     }
 }
